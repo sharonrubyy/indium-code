b9152796ab0fd35928056505c53b155a6d9690f8
Srinivas Roy <84845987+Srinivasroy@users.noreply.github.com>
2021-12-22 10:09:36 +0530

    uploaded

---
+++ Sample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise
+{
+    public class Sample
+    {
+
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a,int b)
+        {
+          return  a - b;
+        }
+        public int Mul(int a, int b)
+        {
+            return a * b; 
+        }
+
+
+        static void Main(string[] args)
+        {
+            Sample s = new Sample();
+            Console.WriteLine(s.Add(10,5));
+            Console.ReadKey();
+        }
+
+    }
+}
